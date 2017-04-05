17ad9ea315072dda2cba6f7091f37ec9dae674a0
cookdani <cookdani@grinnell.edu>
2017-04-05 12:14:14 -0500

    Add files via upload

---
+++ binarysearchadd.java
@@ -0,0 +1,22 @@
+/*
+
+a)
+
+Node<Integer> Node1 = add(root,5);
+Node<Integer> Node2 = add(Node1, 2);
+Node<Integer> Node3 = add(Node1, 8);
+add(Node2, 1);
+add(Node2, 4);
+add(Node3, 6);
+add(Node3, 9);
+
+b)
+Node<Integer> Node1 = add(root,4);
+add(Node1, 2)
+Node<Integer> Node2 = add(Node1,9);
+add(add(Node2, 6), 5);
+add(add(Node2, 15), 20);
+
+c)
+
+add(add(add(add(add(root, 9), 6), 5), 2), 1);
